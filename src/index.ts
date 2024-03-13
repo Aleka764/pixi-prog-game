8669d472afe63f1281ca3e2504e43cf26a47f71f
Aleka764 <ale.caceres1@gmail.com>
2024-03-12 21:06:47 -0300

    Cargue al guerrero y su espada

--- src/index.ts
+++ src/index.ts
@@ -6,26 +6,58 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1280,
+	height: 720
 });
 
+window.addEventListener("resize", ()=>{
+	
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min(scaleX, scaleY);
+	
+	const gameWidth = Math.round(app.screen.width * scale);
+	const gameHeigth = Math.round(app.screen.height * scale);
 
-Loader.shared.add({url: "./stampy.jpg", name: "myStampy"});
-Loader.shared.add({url: "./clampy.png", name: "myClampy"});
+	const marginHorizontal = Math.floor((window.innerWidth - gameWidth) / 2);
+	const marginVertical = Math.floor((window.innerHeight - gameHeigth) / 2);
+
+	app.view.style.width = gameWidth + "px";
+	app.view.style.height = gameHeigth + "px";
+
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";
+
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
+
+});
+window.dispatchEvent(new Event("resize"));
+
+
+Loader.shared.add({url: "./warrior.png", name: "myWarrior"});
+Loader.shared.add({url: "./sword.png", name: "mySword"});
 
 Loader.shared.onComplete.add(()=>{
-	const clampy: Sprite = Sprite.from("myStampy");
-
-	console.log("Hola mundo!", clampy.width, clampy.height);
+	const warrior: Sprite = Sprite.from("myWarrior");
+	const sword: Sprite = Sprite.from("mySword");
+	
 
 	//clampy.anchor.set(0.5);
 
-	clampy.x = 0;
-	clampy.y = 0;
+	warrior.x = 100;
+	warrior.y = 0;
+	sword.x = 100;
+	sword.y = 0;
 
-	app.stage.addChild(clampy);
+	warrior.scale.x = .5;
+	warrior.scale.y = .5;
+	sword.scale.x = .5;
+	sword.scale.y = .5;
 
-})
+	app.stage.addChild(warrior);
+	app.stage.addChild(sword);
+
+});
 
 Loader.shared.load();
