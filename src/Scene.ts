f647be0c7840c2c64933b34b65fceceb504a1f00
Aleka764 <ale.caceres1@gmail.com>
2024-03-14 16:53:23 -0300

    Guerrero animado

--- src/Scene.ts
+++ src/Scene.ts
@@ -1,4 +1,4 @@
-import { Container } from "pixi.js";
+import { AnimatedSprite, Container, Texture } from "pixi.js";
 import { WarriorSword } from "./WarriorSword";
 
 export class Scene extends Container {
@@ -13,5 +13,18 @@ export class Scene extends Container {
         warriorwithsword.position.set(150,130);
 
         this.addChild(warriorwithsword);
+
+        const warriorAnimated: AnimatedSprite = new AnimatedSprite(
+            [
+                Texture.from("WarriorWalk1"),
+                Texture.from("WarriorWalk2"),
+                Texture.from("WarriorWalk3"),
+                Texture.from("WarriorWalk4"),
+                Texture.from("WarriorWalk5"),
+            ], true
+        );
+        warriorAnimated.play();
+        warriorAnimated.animationSpeed = 0.1;
+        this.addChild(warriorAnimated);
     }
 }
