795573e5e902ebbc233147ca1ac1ade7b1a77966
Aleka764 <ale.caceres1@gmail.com>
2024-03-14 22:42:17 -0300

    Cree la escena Level Complete

---
+++ src/SceneLevelComplete.ts
@@ -0,0 +1,9 @@
+import { Container } from "pixi.js"
+
+export class SceneLevelComplete extends Container{
+    constructor()
+    {
+        super();
+        
+    }
+}
--- src/assets.ts
+++ src/assets.ts
@@ -7,8 +7,13 @@ export const assets = [
     {url: "./warriorwalk/walk-2.png", name: "WarriorWalk2"},
     {url: "./warriorwalk/walk-3.png", name: "WarriorWalk3"},
     {url: "./warriorwalk/walk-4.png", name: "WarriorWalk4"},
-    {url: "./warriorwalk/walk-5.png", name: "WarriorWalk5"}
+    {url: "./warriorwalk/walk-5.png", name: "WarriorWalk5"},
 
-    
+    //assets para pantalla Level Complete
+    {url: "./levelcomplete/BoxesBanners/Box_Orange_Rounded.png", name: "BoxOrangeRounded"},
+    {url: "./levelcomplete/BoxesBanners/Banner_Orange.png", name: "BannerOrange"},
+    {url: "./levelcomplete/BoxesBanners/Box_Orange_Square.png", name: "BoxOrangeSquare"},
+
+
 ];
 
