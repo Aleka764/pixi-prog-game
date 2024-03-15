9c7ff9a335872ff1253a38fd063f8e67f88c8ac8
Aleka764 <ale.caceres1@gmail.com>
2024-03-14 22:26:17 -0300

    Cargue assest para crear la pantalla de level complete

--- src/assets.ts
+++ src/assets.ts
@@ -2,10 +2,13 @@
 export const assets = [
     {url: "./warrior.png", name: "myWarrior"},
     {url: "./sword.png", name: "mySword"},
+
     {url: "./warriorwalk/walk-1.png", name: "WarriorWalk1"},
     {url: "./warriorwalk/walk-2.png", name: "WarriorWalk2"},
     {url: "./warriorwalk/walk-3.png", name: "WarriorWalk3"},
     {url: "./warriorwalk/walk-4.png", name: "WarriorWalk4"},
     {url: "./warriorwalk/walk-5.png", name: "WarriorWalk5"}
+
+    
 ];
 
